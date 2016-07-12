6e37431feb532543a4254bdaeda4c9d18fc63e71
FynnLaw <fynnlaw@gmail.com>
2016-07-12 11:04:15 +0800

    reverse log

---
+++ src/main/java/com/fynn/basetest/ReverseSortLog.java
@@ -0,0 +1,71 @@
+package com.fynn.basetest;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ReverseSortLog {
+	public static void main(String args[]){
+		reverseSort();
+	}
+	
+	@SuppressWarnings("static-access")
+	public static void reverseSort(){
+		Map<String,Integer> ipMap = new HashMap<String,Integer>();
+		
+		try {
+			// read file content from file
+            StringBuffer sb= new StringBuffer("");
+            FileReader reader = new FileReader("d://stdout.log");
+            BufferedReader br = new BufferedReader(reader);
+
+            String str = null;
+            while((str = br.readLine()) != null) {
+            	  String ip = str.split(":")[2];
+            	  if(ipMap.containsKey(ip)){
+            		  Integer count = ipMap.get(ip); 
+            		  count = count + 1;
+            		  ipMap.put(ip, count);
+            	  }else{
+            		  ipMap.put(ip, 1);
+            	  }
+            }
+            
+            br.close();
+            reader.close();
+            
+            int order = 1;
+            while(!ipMap.isEmpty()){
+            	Integer maxCount = new Integer(0);
+                String maxKey = "";
+                
+                for (String key : ipMap.keySet()) {
+    	              Integer count = ipMap.get(key);
+    	              if(count.compare(count, maxCount) >= 0){
+    	            	  maxCount = count;
+    	            	  maxKey = key;
+    	              }
+            	}
+                
+                sb.append((order++) + ":" + maxKey + ",appear times:" + ipMap.get(maxKey) +"\n");
+                ipMap.remove(maxKey);
+            }
+            
+            // write string to file
+            FileWriter writer = new FileWriter("d://stdoutReverse.log");
+            BufferedWriter bw = new BufferedWriter(writer);
+            bw.write(sb.toString());
+            bw.close();
+            writer.close();
+		} catch(FileNotFoundException e) {
+            e.printStackTrace();
+      }catch(IOException e) {
+            e.printStackTrace();
+      }
+	}
+}
